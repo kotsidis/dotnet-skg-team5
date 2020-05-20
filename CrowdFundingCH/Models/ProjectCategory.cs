660c105e80dfcfaf2fe41eeaaee765b7b2e275a1
giorgos kotsidis <kotsidisgiorgos@hotmail.gr>
2020-05-20 12:13:16 +0300

    Add project files.

---
+++ CrowdFundingCH/Models/ProjectCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace CrowdFundingCH.Models
+{
+    public enum ProjectCategory
+    {
+        Myth = 1,
+        Tech = 2,
+        Arts = 3,
+        Football = 4,
+        Fishing = 5
+    }
+}
